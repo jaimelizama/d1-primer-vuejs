eb613a40f698cc3dc9a9a1c358ee5e5b265df890
Jaime Lizama R <jlizama@poptimize.cl>
2021-08-13 00:02:47 -0400

    Mi primer Vue
    Desafío 1 Primer Vue

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,53 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container mt-5">
+    <h2 class="my-3 row justify-content-center">Tabla de Usuarios</h2>
+    <table class="table table-bordered table-hover">
+      <thead>
+        <tr>
+          <th scope="col">#</th>
+          <th scope="col">Nombre</th>
+          <th scope="col">Apellido</th>
+          <th scope="col">RUN</th>
+          <th scope="col">Fecha de Nacimiento</th>
+          <th scope="col">Edad</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(persona, $index) in personas" :key="$index">
+          <td>{{ $index + 1 }}</td>
+          <td>{{ persona.nombre }}</td>
+          <td>{{ persona.apellido }}</td>
+          <td>{{ persona.run }}</td>
+          <td>{{ persona.fechaDeNacimiento }}</td>
+          <td>{{ persona.edad }}</td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  data: () => ({
+    personas: [
+      {
+        nombre: 'Jaime',
+        apellido: 'Lizama',
+        run: '1.234.567-8',
+        fechaDeNacimiento: '01-01-2020',
+        edad: 20
+      },
+      {
+        nombre: 'Coté',
+        apellido: 'Pop',
+        run: '1.234.567-8',
+        fechaDeNacimiento: '01-01-2020',
+        edad: 20
+      }
+    ]
+  })
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style scoped></style>
